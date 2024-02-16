97e37ef5d3892f8ca1203aa5c0ec42e55d0de2b9
Pranab Ghosh <pranabit6@gmail.com>
2024-02-16 12:05:44 +0530

    added some files

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ let Person = {
     age: 30,
 };
 
+/*
 let car: {
     color: string;
     brand: string;
@@ -26,4 +27,27 @@ article = {
     author: "Stella",
     content: "Hello World",
     title: "My First Article",
+};
+
+The way of declaring object type is not recommended. We can use type alias or interface to declare object type.
+Example of type alias
+*/
+
+type Article = {
+    author: string;
+    content: string;
+    title: string;
+    image?: string; // Optional property
+};
+
+let article: Article = {
+    author: "Stella",
+    content: "Hello World",
+    title: "My First Article",
+};
+
+let post: Article = {
+    author: "Sam",
+    content: "Hello World",
+    title: "My Second Article",
 };
